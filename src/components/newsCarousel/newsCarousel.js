656ccc234bdd2d20e7acd5f9df93cf78692e873e
Banchee501 <rosstactical373@gmail.com>
2022-11-09 02:09:27 +0200

    create slider and styles

--- src/components/newsCarousel/newsCarousel.js
+++ src/components/newsCarousel/newsCarousel.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Carousel } from 'react-bootstrap';
 
+import './carousel.scss';
+
 import firstSlide from "../../resources/img/firstSlide.jpg";
 // import secondSlide from "../../resources/img/secondSlide.png";
 // import thirdSlide from "../../resources/img/thirdSlide.png";
@@ -10,6 +12,36 @@ const newsCarousel = () => {
     return (
         <Container fluid>
             <Carousel>
+                <Carousel.Item>
+                    <img
+                        className=""
+                        src={firstSlide}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        Андрій Єрмак: «В нас інші прізвища, ніж у творців Мінських угод. Людей, які б таке допускали, в нашій команді немає»
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className=""
+                        src={firstSlide}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        Андрій Єрмак: «В нас інші прізвища, ніж у творців Мінських угод. Людей, які б таке допускали, в нашій команді немає»
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className=""
+                        src={firstSlide}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        Андрій Єрмак: «В нас інші прізвища, ніж у творців Мінських угод. Людей, які б таке допускали, в нашій команді немає»
+                    </Carousel.Caption>
+                </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className=""
