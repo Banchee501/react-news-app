3f38447a78854538aa8f6633d9652615665a5b8a
Banchee501 <rosstactical373@gmail.com>
2022-11-21 02:02:04 +0200

    fixed OtherNews.js, create footer

--- src/components/otherNews/OtherNews.js
+++ src/components/otherNews/OtherNews.js
@@ -12,6 +12,36 @@ const Politics = () => {
                             <div className='title title_fz24'>Колонки</div>
                             <Link to="/" className='fz14'>Всі колонки</Link>
                         </div>
+                        <div className="news__col-items">
+                            <div className="news__col-item">
+                                <img src="" alt="" />
+                                <div className="news__col-name"></div>
+                                <div className="news__col-employment"></div>
+                                <div className="news__col-title"></div>
+                                <div className="date"></div>
+                            </div>
+                            <div className="news__col-item">
+                                <img src="" alt="" />
+                                <div className="news__col-name"></div>
+                                <div className="news__col-employment"></div>
+                                <div className="news__col-title"></div>
+                                <div className="date"></div>
+                            </div>
+                            <div className="news__col-item">
+                                <img src="" alt="" />
+                                <div className="news__col-name"></div>
+                                <div className="news__col-employment"></div>
+                                <div className="news__col-title"></div>
+                                <div className="date"></div>
+                            </div>
+                            <div className="news__col-item">
+                                <img src="" alt="" />
+                                <div className="news__col-name"></div>
+                                <div className="news__col-employment"></div>
+                                <div className="news__col-title"></div>
+                                <div className="date"></div>
+                            </div>
+                        </div>
                     </div>
                     <button className='fz16 load__more'>
                         Всі колонки
@@ -21,6 +51,11 @@ const Politics = () => {
                     <div className="news__politics-head">
                         <div className="title title_fz32">Політика</div>
                         <Link to="/" className='fz14'>Всі новини розділу</Link>
+                    </div>
+                    <div className="news__politics-block">
+                        <img src="" alt="" />
+                        <div className="date"></div>
+                        <div className="news__politics-text"></div>
                     </div>
                 </div>
             </div>
