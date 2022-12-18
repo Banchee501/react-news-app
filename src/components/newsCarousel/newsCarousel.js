9ad6797f5ddd80b047684d7aba68a1081ee0def9
Banchee501 <rosstactical373@gmail.com>
2022-12-18 23:40:36 +0200

    refactoring code, fixed

--- src/components/newsCarousel/newsCarousel.js
+++ src/components/newsCarousel/newsCarousel.js
@@ -28,26 +28,29 @@ export default class AsNavFor extends Component {
     render() {
       return (
         <div className='container'>
+          <div className='slide'>
+            {Slider.customPaging}
+          </div>
           <Slider
           className='carousel-slide'
-            asNavFor={this.state.nav2}
-            ref={slider => (this.slider1 = slider)}>
-                <div className='carousel-slide-item'>
-                    <img src={firstSlide} alt="First slide"/>
-                    <p className="fz40">Андрій Єрмак: «В нас інші прізвища, ніж у творців Мінських угод. Людей, які б таке допускали, в нашій команді немає»</p>
-                </div>
-                <div className='carousel-slide-item'>
-                    <img src={secondSlide} alt="Second slide" />
-                    <p className="fz40">Оприлюднено супутникові знімки аеродрому біля Новофедорівки: видно місця ураження</p>
-                </div>
-                <div className='carousel-slide-item'>
-                    <img src={thirdSlide} alt="Third slide" />
-                    <p className="fz40">Вибухи на білоруському аеродромі "Зябрівка": оприлюднені причини</p>
-                </div>
-                <div className='carousel-slide-item'>
-                    <img src={fourthSlide} alt="Fourth slide" />
-                    <p className="fz40">Сейм Латвії визнав рф країною-спонсором тероризму</p>
-                </div>
+          asNavFor={this.state.nav2}
+          ref={slider => (this.slider1 = slider)}>
+              <div className='carousel-slide-item'>
+                  <img src={firstSlide} alt="First slide"/>
+                  <p className="fz40">Андрій Єрмак: «В нас інші прізвища, ніж у творців Мінських угод. Людей, які б таке допускали, в нашій команді немає»</p>
+              </div>
+              <div className='carousel-slide-item'>
+                  <img src={secondSlide} alt="Second slide" />
+                  <p className="fz40">Оприлюднено супутникові знімки аеродрому біля Новофедорівки: видно місця ураження</p>
+              </div>
+              <div className='carousel-slide-item'>
+                  <img src={thirdSlide} alt="Third slide" />
+                  <p className="fz40">Вибухи на білоруському аеродромі "Зябрівка": оприлюднені причини</p>
+              </div>
+              <div className='carousel-slide-item'>
+                  <img src={fourthSlide} alt="Fourth slide" />
+                  <p className="fz40">Сейм Латвії визнав рф країною-спонсором тероризму</p>
+              </div>
           </Slider>
           <Slider
             className='carousel-slick-slide'
